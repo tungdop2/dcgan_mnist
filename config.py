adee57d6a1f0335aa47712949520cabe1361efee
Qtieee <tungdop2@gmail.com>
2022-06-19 21:35:08 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from easydict import EasyDict as edict
 
 config = edict()
-config.num_epochs = 200
+config.num_epochs = 300
 config.num_classes = 1
 config.num_g_filters = 64
 config.num_d_filters = 64
